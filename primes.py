5d5868d8b1b64c41db3c2487b49df7bd28243751
Ibrahim <k22013981@nmslab-005828.nms.kcl.ac.uk>
2023-09-29 15:23:08 +0100

    Solution

--- primes.py
+++ primes.py
@@ -3,4 +3,26 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError("Number can't be 0 or less")
+
+
+    num = 2
+    list.append(num)
+
+    while(len(list) < number_of_primes):
+
+        prime = True
+        num+=1
+        for i in list:
+            if num%i == 0:
+                prime = False
+            
+        if prime:
+            list.append(num)
+        
+
+
+    
     return list
